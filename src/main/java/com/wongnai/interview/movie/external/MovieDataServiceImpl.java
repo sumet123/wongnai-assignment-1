71d98720db811d2fbfe00108b5b2e15f35ea8cd6
Sumet Chorthongdee <sumetchor@gmail.com>
2019-11-14 20:40:59 +0700

    Implement fetchAll() method (Problem 1)

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,13 @@
 package com.wongnai.interview.movie.external;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -24,6 +27,19 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
 
-		return null;
+		// Get raw json string from MOVIE_DATA_URL
+		final String response = restTemplate.getForObject(MOVIE_DATA_URL, String.class);
+
+		// Mapping json string to MoviesResponse ArrayList
+		MoviesResponse moviesResponse = new MoviesResponse();
+		try {
+			MovieData[] moviesArray = objectMapper.readValue(response, MovieData[].class);
+			for(MovieData movie : moviesArray){
+				moviesResponse.add(movie);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return moviesResponse;
 	}
 }
