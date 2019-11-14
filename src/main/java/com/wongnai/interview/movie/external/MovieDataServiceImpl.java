6a677593618ca3cd406d6c190558fb4e85d0a241
Sumet Chorthongdee <sumetchor@gmail.com>
2019-11-14 22:52:21 +0700

    Remove unused import

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,6 +1,5 @@
 package com.wongnai.interview.movie.external;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
